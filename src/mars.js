eae469a31fbb55d5e61d4d00991414038352cb8d
hungaria <hungaria@student.21-school.ru>
2023-11-23 15:08:13 +0300

    add md

---
+++ src/mars.js
@@ -0,0 +1,3 @@
+const mySpacecraft = new Spacecraft('Odyssey', 'Hydrogen');
+const myMarsRover = new MarsRover('Curiosity', 'Advanced GPS');
+const missionControl = new MissionControl('Mars Exploration', '2023-09-30', mySpacecraft, myMarsRover);
