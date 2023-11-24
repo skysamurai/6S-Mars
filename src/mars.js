706acfcfdfa5ca736f61d0c71cac60bb2f4d5a41
hungaria <hungaria@student.21-school.ru>
2023-11-24 16:51:59 +0300

    add all methods

--- src/mars.js
+++ src/mars.js
@@ -21,11 +21,31 @@ function Spacecraft(name, fueltype) {
     } else {
       console.log(`Корабль ${this.name} находится в космосе!`);
     }
-  }
+  };
+
+  this.land = function(destination) {
+    console.log(`Экипажу ${this.name} подготовиться к посадке на ${destination}.`);
+    this.fuelLevel -= 10;
+    this.isInSpace = false;
+    console.log(`Поздравляем с успешной посадкой на ${destination}.`);
+    };
   
-  this.land(destination){};
-  this.checkFuel(){};
-  this.refuel(amount){};
+  this.checkFuel = function(){
+    console.log(`Текущий уровень топлива корабля ${this.name} составляет ${this.fuelLevel}`)
+  };
+
+  this.refuel = function(amount) {
+    this.fuelLevel += amount
+    if (this.fuelLevel > 100) {
+      console.log(`Баки корабля ${this.name} не вместят данный объем топлива.`);
+      console.log(`Корабль полностью заправлен. ${this.fuelLevel - 100} топлива возвращено в порт.`);
+      this.fuelLevel = 100;
+    } else if (this.fuelLevel < 100) {
+      console.log(`Корабль заправлен на ${this.fuelLevel}%, для отправления требуется дозаправка.`)
+    } else {
+      console.log(`Корабль полностью заправлен.`);
+    }
+  }
 }
 
 function MarsRover(model, navigationSystem) {
@@ -33,18 +53,59 @@ function MarsRover(model, navigationSystem) {
   this.navigationSystem = navigationSystem;
   this.dataCollected = [];
 
-  this.move(direction){};
-  this.collectData(dataType){};
-  this.sendDataToEarth(){};
+  this.move = function(direction) {
+    console.log(`Марсоход ${this.model} отправлен в направлении ${direction}.`);
+  };
+  this.collectData = function(dataType) { 
+    console.log(`Марсоход ${this.model} собирает данные формата ${dataType}.`);
+    this.dataCollected.push(dataType);
+  };
+  this.sendDataToEarth = function() {
+    console.log(`Данные с марсохода ${this.model} отправлены на Землю. База данных очищена.`);
+    this.dataCollected = [];
+  };
 }
-function MissionControl(missionName, launchDate) {
+
+function MissionControl(missionName, launchDate, spacecraft, marsRover) {
   this.missionName = missionName;
   this.launchDate = launchDate;
-  this.spacecraft;
-  this.marsRover;
+  this.spacecraft = spacecraft;
+  this.marsRover = marsRover;
 
-  this.initiateLaunch(){};
-  this.deployMarsRover(){};
-  this.coordinateMission(){};
-  this.monitorMissionProgress(){};
+  this.initiateLaunch = function() {
+    this.spacecraft.launch();
+  }
+  this.deployMarsRover = function() {
+    this.marsRover.move('поверхность Марса');
+
+  }
+  this.coordinateMission = function() {
+    console.log(`Миссия ${this.missionName} началась...`);
+    console.log(`Марсоход ${this.marsRover.model} начинает сбор данных.`);
+    this.marsRover.collectData('Photo');
+    this.marsRover.collectData('Temperature');
+    this.marsRover.collectData('Air composition');
+    this.marsRover.sendDataToEarth();
+  }
+
+  this.monitorMissionProgress = function() {
+    console.log(`Миссия ${this.missionName}.`);
+    console.log(`${this.launchDate} c земли отправлен марсоход ${this.marsRover.model} на корабле ${this.spacecraft.name}`);
+    this.spacecraft.checkFuel();
+  };
 }
+
+/* mySpacecraft.launch();
+mySpacecraft.land('Mars');
+mySpacecraft.checkFuel();
+mySpacecraft.refuel(30);
+mySpacecraft.checkFuel();
+
+myMarsRover.move('Запад');
+myMarsRover.collectData('JPG');
+myMarsRover.sendDataToEarth(); */
+
+missionControl.initiateLaunch();
+missionControl.deployMarsRover();
+missionControl.coordinateMission();
+missionControl.monitorMissionProgress();
